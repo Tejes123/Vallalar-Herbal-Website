8cca67a8ebd728b8c2978176ee5b6f3e8d62ebf9
Tejes123 <115523948+Tejes123@users.noreply.github.com>
2023-10-24 19:07:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
-
 function addData()
-    {
+    {   
         var fname = document.getElementById("firstname").value;
         var lname = document.getElementById("lastname").value;
-        var uemail = document.getElementById("email").value;
-        var upass = document.getElementById("password").value;   
-        if (fname && lname && uemail && upass) {
+        var uemail = document.getElementById("email").value.trim();
+        var upass = document.getElementById("password").value; 
+        var emaflag =0;
+        var passflag = 0;
+        var specialchar = "!@#$%^&*~`-_+=/?><,";
+        var passflagtemp = 0;
+        var emailvalue = localStorage.getItem(uemail);
+        for (i=0; i < specialchar.length; i++) {
+            if (upass.includes(specialchar[i])) {
+                passflagtemp = 1;
+                break;
+            }
+        }
+        if (uemail && uemail.includes('@') && (uemail.match(/@\D+.com/) || uemail.match(/@\D+.in/) || uemail.match(/@\D+.net/) )) {
+            emaflag = 1;
+        }
+        if (upass.length > 8 && passflagtemp && upass.match(/\d/)) {
+            passflag = 1;
+        }
+        if (emailvalue == undefined) {
+        if (fname && lname && uemail && upass)  {
+        if (emaflag && passflag) {
             let object = { "firstname":fname, "lastname":lname, "useremail":uemail, "userpwd":upass };
             localStorage.setItem(uemail, JSON.stringify(object));
-            alert("Sign In Successful! " + fname + lname);
+            alert("Sign In Successful! " + fname +" " + lname);
+            firstn = JSON.parse(localStorage.getItem(uemail)).firstname;
+            lastn = JSON.parse(localStorage.getItem(uemail)).lastname;
         }
+        else if (emaflag == 0 && passflag == 1 ) {
+            alert("Your Email is invalid. \nPlease check if your email includes '@' sign and proper extension.");
+        }
+        else if(emaflag == 1 && passflag == 0) {
+            alert("Your Password is invalid.\n It does not meet up the security requirements mentioned.")
+        }
+        else {
+            alert("Both your email and password are invalid. \nPlease recheck.")
+        }
+    }
         else {
             alert("Some Fields are Empty.");
         }
     }
-
+    else {
+        alert("This email already exists. Please try a new email.");
+    }
+    }
 
 function checkData()
     {
@@ -32,6 +65,8 @@ function checkData()
                 if(existpass == givenpass) {
                     var fullname = JSON.parse(obj).firstname + " " + JSON.parse(obj).lastname;
                     alert("Hurray! You have Log In Successfully!\nWelcome! " + fullname );
+                    firstn = JSON.parse(localStorage.getItem(uemail)).firstname;
+                    lastn = JSON.parse(localStorage.getItem(uemail)).lastname;
                 }    
                 else {
                     alert("Password is Incorrect");
@@ -43,6 +78,11 @@ function checkData()
             alert("Some Fields are Empty");
         }
     }
+    
+
+
+
+
 
     
 
